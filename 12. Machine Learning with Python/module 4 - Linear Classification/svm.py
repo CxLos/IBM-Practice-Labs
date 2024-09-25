9abced104049245b7f9102de549125f4a402c79f
cxlos <bautistac_29@outlook.com>
2024-09-25 16:08:41 -0500

    Starting svm lab

---
+++ 12. Machine Learning with Python/module 4 - Linear Classification/svm.py
@@ -0,0 +1,129 @@
+
+# =============================== Imports ============================= #
+
+from sklearn import metrics
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import pylab as pl
+import numpy as np
+import os
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+from dash.development.base_component import Component
+
+# ========================== Load Data ==========================
+
+# df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv')
+
+current_dir = os.getcwd()
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = 'data/.csv'
+file_path = os.path.join(script_dir, data_path)
+# df = pd.read_csv(file_path)
+
+# print(current_dir)
+# print(script_dir)
+
+# ========================== Data Exploration ========================== #
+
+
+
+# ========================== Data Visualization ========================== #
+
+
+
+# =============================== Dash App =============================== #
+
+app = dash.Dash(__name__)
+server= app.server
+
+app.layout = html.Div(children=[ 
+
+    html.Div(className='divv', children=[ 
+        
+        html.H1('Support Vector Machine (SVM)', 
+        className='title'),
+
+        html.A(
+        'Repo',
+        href='https://github.com/CxLos/IBM-Practice-Labs/blob/main/12.%20Machine%20Learning%20with%20Python/module%203%20-%20Classification/svm.py',
+        className='btn')
+    ]),
+
+# ROW 1
+html.Div(
+    className='row1',
+    children=[
+        html.Div(
+            className='graph1',
+            children=[
+                dcc.Graph(
+                    id='graph1',
+
+
+                )
+            ]
+        ),
+        html.Div(
+            className='graph2',
+            children=[
+                dcc.Graph(
+
+                )
+            ]
+        )
+    ]
+),
+
+# ROW 2
+html.Div(
+    className='row2',
+    children=[
+        html.Div(
+            className='graph1',
+            children=[
+                dcc.Graph(
+
+                )
+            ]
+        ),
+        html.Div(
+            className='graph2',
+            children=[
+                dcc.Graph(
+
+                )
+            ]
+        )
+    ]
+),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=
+                   True)
+                #    False)
+
+# ================================ Export Data =============================== #
+
+# updated_path = 'data/.csv'
+# data_path = os.path.join(script_dir, updated_path)
+# df.to_csv(data_path, index=False)
+# print(f"DataFrame saved to {data_path}")
+
+# ============================== Update Dash ================================ #
+
+# pip install dash --upgrade
+# pip install dash-core-components --upgrade
+# pip install dash-html-components --upgrade
+# pip install dash-renderer --upgrade
+
+# ========================================================================== #
