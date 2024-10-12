13a4e57ccdd1b4048dfd4b651c6c48b0d35a7fb9
cxlos <bautistac_29@outlook.com>
2024-10-11 21:08:07 -0500

    finished data collection lab

--- Capstone Project 2/module 1 - Intro/data_collection_spacex.py
+++ Capstone Project 2/module 1 - Intro/data_collection_spacex.py
@@ -1,9 +1,6 @@
 
 # =============================== Imports ============================= #
 
-from sklearn.cluster import KMeans 
-from sklearn.datasets import make_blobs 
-from sklearn.metrics import f1_score, jaccard_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
@@ -12,52 +9,187 @@ from mpl_toolkits.mplot3d import Axes3D
 import plotly.graph_objects as go
 import plotly.colors as pc
 import plotly.express as px
+import datetime
 import itertools
+import requests
 import pandas as pd
 import pylab as pl
 import numpy as np
+import sys
+import re
 import os
 import dash
 from dash import dcc, html
 
 # ============================= Load Data ============================= #
 
+# Setting this option will print all collumns of a dataframe
+pd.set_option('display.max_columns', None)
+# Setting this option will print all of the data in a feature
+pd.set_option('display.max_colwidth', None)
+
+# Takes the dataset and uses the rocket column to call the API and append the data to the list
+def getBoosterVersion(data):
+    for x in data['rocket']:
+       if x:
+        response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+        BoosterVersion.append(response['name'])
+
+# Takes the dataset and uses the launchpad column to call the API and append the data to the list
+def getLaunchSite(data):
+    for x in data['launchpad']:
+       if x:
+         response = requests.get("https://api.spacexdata.com/v4/launchpads/"+str(x)).json()
+         Longitude.append(response['longitude'])
+         Latitude.append(response['latitude'])
+         LaunchSite.append(response['name'])
+
+# Takes the dataset and uses the payloads column to call the API and append the data to the lists
+def getPayloadData(data):
+    for load in data['payloads']:
+       if load:
+        response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+        PayloadMass.append(response['mass_kg'])
+        Orbit.append(response['orbit'])
+
+# Takes the dataset and uses the cores column to call the API and append the data to the lists
+def getCoreData(data):
+    for core in data['cores']:
+            if core['core'] != None:
+                response = requests.get("https://api.spacexdata.com/v4/cores/"+core['core']).json()
+                Block.append(response['block'])
+                ReusedCount.append(response['reuse_count'])
+                Serial.append(response['serial'])
+            else:
+                Block.append(None)
+                ReusedCount.append(None)
+                Serial.append(None)
+            Outcome.append(str(core['landing_success'])+' '+str(core['landing_type']))
+            Flights.append(core['flight'])
+            GridFins.append(core['gridfins'])
+            Reused.append(core['reused'])
+            Legs.append(core['legs'])
+            LandingPad.append(core['landpad'])
+
+spacex_url="https://api.spacexdata.com/v4/launches/past"
+response = requests.get(spacex_url)
+# print(response.content)
+
+static_json_url='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+# print(response.status_code)
+
+# Use json_normalize meethod to convert the json result into a dataframe
+data = pd.json_normalize(response.json())
+# print(data.head())
+
 # df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
 
 current_dir = os.getcwd()
 script_dir = os.path.dirname(os.path.abspath(__file__))
-data_path = 'data/.csv'
+data_path = 'data/spacex.csv'
 file_path = os.path.join(script_dir, data_path)
-df = pd.read_csv(file_path)
+# df = pd.read_csv(file_path)
 
 # print(current_dir)
 # print(script_dir)
 
+
+
 # ========================== Data Pre Processing ========================== #
 
+# Lets take a subset of our dataframe keeping only the features we want and the flight number, and date_utc.
+data = data[['rocket', 'payloads', 'launchpad', 'cores', 'flight_number', 'date_utc']]
 
+# We will remove rows with multiple cores because those are falcon rockets with 2 extra rocket boosters and rows that have multiple payloads in a single rocket.
+data = data[data['cores'].map(len)==1]
+data = data[data['payloads'].map(len)==1]
 
-# ========================== K - Means ========================== #
+# Since payloads and cores are lists of size 1 we will also extract the single value in the list and replace the feature.
+data['cores'] = data['cores'].map(lambda x : x[0])
+data['payloads'] = data['payloads'].map(lambda x : x[0])
 
+# We also want to convert the date_utc to a datetime datatype and then extracting the date leaving the time
+data['date'] = pd.to_datetime(data['date_utc']).dt.date
 
+# Using the date we will restrict the dates of the launches
+data = data[data['date'] <= datetime.date(2020, 11, 13)]
 
-# ========================== Train / Test Split ========================== #
+# ==========================  ========================== #
 
-# X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
-# print ('Train set:', X_train.shape,  y_train.shape)
-# print ('Test set:', X_test.shape,  y_test.shape)
+#Global variables 
+BoosterVersion = []
+PayloadMass = []
+Orbit = []
+LaunchSite = []
+Outcome = []
+Flights = []
+GridFins = []
+Reused = []
+Legs = []
+LandingPad = []
+Block = []
+ReusedCount = []
+Serial = []
+Longitude = []
+Latitude = []
 
-# ========================== Modeling ========================== #
+# Call getBoosterVersion
+getBoosterVersion(data)
+# print(BoosterVersion[0:5])
 
+# Call getLaunchSite
+getLaunchSite(data)
 
+# Call getPayloadData
+getPayloadData(data)
 
-# ========================== Data Visualization ========================== #
+# Call getCoreData
+getCoreData(data)
 
+# Finally lets construct our dataset using the data we have obtained. We we combine the columns into a dictionary.
+launch_dict = {
+'FlightNumber': list(data['flight_number']),
+'Date': list(data['date']),
+'BoosterVersion':BoosterVersion,
+'PayloadMass':PayloadMass,
+'Orbit':Orbit,
+'LaunchSite':LaunchSite,
+'Outcome':Outcome,
+'Flights':Flights,
+'GridFins':GridFins,
+'Reused':Reused,
+'Legs':Legs,
+'LandingPad':LandingPad,
+'Block':Block,
+'ReusedCount':ReusedCount,
+'Serial':Serial,
+'Longitude': Longitude,
+'Latitude': Latitude
+}
 
+# Then, we need to create a Pandas data frame from the dictionary launch_dict.
+df = pd.DataFrame(launch_dict)
+# print(df.head())
+
+# ========================== Filtering ========================== #
+
+# Hint data['BoosterVersion']!='Falcon 1'
+df_falcon9 = df[df['BoosterVersion'] == 'Falcon 9']
+
+# Reser 'FlightNumber' column
+df_falcon9.loc[:,'FlightNumber'] = list(range(1, df_falcon9.shape[0]+1))
+
+print('Missing Values Before: \n', df_falcon9.isnull().sum())
+
+# Calculate below the mean for the PayloadMass using the .mean(). Then use the mean and the .replace() function to replace `np.nan` values in the data with the mean you calculated.
+mean = df_falcon9['PayloadMass'].mean()
+df_falcon9['PayloadMass'] = df_falcon9['PayloadMass'].replace(np.nan, mean)
+
+print('Missing Values After: \n', df_falcon9.isnull().sum())
 
 # ========================== DataFrame Table ========================== #
 
-fig_head = go.Figure(data=[go.Table(
+df_table = go.Figure(data=[go.Table(
     # columnwidth=[50, 50, 50],  # Adjust the width of the columns
     header=dict(
         values=list(df.columns),
@@ -77,7 +209,7 @@ fig_head = go.Figure(data=[go.Table(
     )
 )])
 
-fig_head.update_layout(
+df_table.update_layout(
     margin=dict(l=50, r=50, t=30, b=40),  # Remove margins
     height=400,
     # width=1500,  # Set a smaller width to make columns thinner
@@ -121,7 +253,7 @@ html.Div(
             children=[
                 dcc.Graph(
                     className='data',
-                    figure=fig_head
+                    figure=df_table
                 )
             ]
         )
@@ -182,9 +314,9 @@ html.Div(
 
 # ================================ Export Data =============================== #
 
-# updated_path = 'data/.csv'
+# updated_path = 'data/spacex.csv'
 # data_path = os.path.join(script_dir, updated_path)
-# cust_df.to_csv(data_path, index=False)
+# data.to_csv(data_path, index=False)
 # print(f"DataFrame saved to {data_path}")
 
 # ============================== Update Dash ================================ #
